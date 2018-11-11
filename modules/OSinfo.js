1e5faaba5cfb21b0bbd7f1f50ed1c33cc0318a49
Michał Skwira <michalskwira@yahoo.com>
2018-11-11 07:20:26 +0100

    Add secToHour.js

---
+++ modules/OSinfo.js
@@ -0,0 +1,22 @@
+var os = require('os');
+var formatTime = require('./formatTime');
+
+function getOSinfo() {
+    var type = os.type();
+    var release = os.release();
+    if (type === 'Darwin') {
+        type = 'OSX';
+    } else if (type === 'Window_NT') {
+        type = 'Windows';
+    }
+    var cpu = os.cpus()[0].model;
+    var userInfo = os.userInfo();
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu)
+    console.log('Uptime:', formatTime.print());
+}
+
+exports.print = getOSinfo;
